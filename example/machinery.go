b1f91795d52808153fa01f01ea8f98a33efa1b66
Matt Way <mway@uber.com>
2017-06-20 20:32:04 -0400

    Fix example imports

--- example/machinery.go
+++ example/machinery.go
@@ -5,13 +5,13 @@ import (
 	"os"
 	"time"
 
-	"github.com/RichardKnop/machinery/v1"
-	"github.com/RichardKnop/machinery/v1/config"
-	"github.com/RichardKnop/machinery/v1/log"
-	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/mway/machinery/v1"
+	"github.com/mway/machinery/v1/config"
+	"github.com/mway/machinery/v1/log"
+	"github.com/mway/machinery/v1/tasks"
 	"github.com/urfave/cli"
 
-	exampletasks "github.com/RichardKnop/machinery/example/tasks"
+	exampletasks "github.com/mway/machinery/example/tasks"
 )
 
 var (

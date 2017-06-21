9355a137d70d209a1ee956214a4a0859a89aa0a1
Matt Way <mway@uber.com>
2017-06-20 20:33:44 -0400

    Fix integration test imports

--- integration-tests/worker_only_consumes_registered_tasks_test.go
+++ integration-tests/worker_only_consumes_registered_tasks_test.go
@@ -8,9 +8,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/RichardKnop/machinery/v1"
-	"github.com/RichardKnop/machinery/v1/config"
-	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/mway/machinery/v1"
+	"github.com/mway/machinery/v1/config"
+	"github.com/mway/machinery/v1/tasks"
 )
 
 func TestWorkerOnlyConsumesRegisteredTaskAMQP(t *testing.T) {
